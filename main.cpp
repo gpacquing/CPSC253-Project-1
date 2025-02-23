39c531a3e7800e3aa09b4fb0606514339d769dbc
Gabriel Pacquing <gdpacquing@csu.fullerton.edu>
2025-02-22 16:35:13 -0800

    Fixed logic issue with validating positive integer input

--- main.cpp
+++ main.cpp
@@ -97,22 +97,24 @@ class supasecret {
 	int main() {
 		std::string crypt_opt_;
 		size_t key_;
+		int temp_key_;
 		std::string input_file_;
 		std::string output_file_;
 	
 		std::cout << "Hello, would you like to encrypt or decrypt a file? e/d " << std::endl;
 		std::cout << "Enter Here: ";
 		std::cin >> crypt_opt_;
-		if (crypt_opt_ != "e" || "d") {
-			std::cout << "Invalid input. Options are e to encrypt or d to decrypt a file."
+		if (crypt_opt_ != "e" && crypt_opt_ != "d") {
+			std::cout << "Invalid input. Options are e to encrypt or d to decrypt a file.";
 			return 1;
 		}
 		std::cout << "Please enter a key (positive integer): " << std::endl;
-		std::cin >> key_;
-		if (key_ <= 0) {
+		std::cin >> temp_key_;
+		if (temp_key_ <= 0) {
 			std::cout << "Invalid key. Key must be a positive integer." << std::endl;
-			return 1;  
+			return 1;
 		}
+		key_ = static_cast<size_t>(temp_key_);
 		std::cout << "Enter the name of the file you wish to input: " << std::endl;
 		std::cin >> input_file_;
 		std::cout << "Enter the name of the file you wish to output: " << std::endl;
